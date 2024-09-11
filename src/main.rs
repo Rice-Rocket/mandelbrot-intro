5fdb3e15174e0cd15890e9b41ad71b33fc27d391
Killian Hourihan <kllnhrhn@gmail.com>
2024-09-11 16:44:52 -0700

    Documented everything and simplified the main code

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,70 @@
-use complex::Complex;
-use image::{buffer::ConvertBuffer, GenericImage, ImageBuffer, Rgb, Rgb32FImage, RgbImage, Rgba32FImage};
-use rand::{thread_rng, Rng};
+use util::{Color, Complex, MandelbrotImage, Point};
 
-mod complex;
+mod util;
 
-const NUM_SAMPLES: usize = 100000000;
+// The number of iterations used to find the edge of the mandelbrot set.
+// Note that larger numbers here will be slower to calculate, but lead to more detailed results.
 const NUM_ITERATIONS: u32 = 100;
+// The resolution of the image in pixels.
+// Final image will have IMAGE_SIZE x IMAGE_SIZE pixels.
 const IMAGE_SIZE: u32 = 2048;
 
-fn mandelbrot(c: Complex<f32>, n: u32) -> f32 {
+fn mandelbrot(c: Complex<f32>) -> f32 {
+    // Initialize `z` at `c`.
     let mut z = c;
 
-    for _ in 0..n {
+    // Loop through the following for NUM_ITERATIONS times.
+    for _ in 0..NUM_ITERATIONS {
+        // Update `z` via the Mandelbrot function
+        // z = z² + c
         z = z * z + c;
+
+        // If `z` escapes the set, exit and return 1
         if z.abs() > 2.0 {
             return 1.0;
         }
     }
 
+    // If `z` never escapes the set, return 0
     0.0
 }
 
 #[inline]
 fn transform(c: Complex<f32>) -> Complex<f32> {
+    // Transform c to be in [-2, 2] in both real and imaginary axes
     c * 4.0 - 2.0
 }
 
 fn main() {
-    let mut im = RgbImage::new(IMAGE_SIZE, IMAGE_SIZE);
-    let mut rng = thread_rng();
+    // Create a new image with a width and height of IMAGE_SIZE.
+    let mut im = MandelbrotImage::new(IMAGE_SIZE, IMAGE_SIZE);
         
-    for _ in 0..NUM_SAMPLES {
-        let r1 = rng.gen::<f32>();
-        let r2 = rng.gen::<f32>();
+    // Loop through the x and y dimensions of the image.
+    for x in 0..IMAGE_SIZE {
+        for y in 0..IMAGE_SIZE {
+            // Instantiate a new point for the current pixel.
+            let p = Point::new(x, y);
 
-        let mut c = Complex::new(r1, r2);
-        c = transform(c);
+            // Convert the pixel position to u, v coordinates in [0, 1] based on the image
+            // size.
+            let uv = p.to_uv(IMAGE_SIZE);
 
-        let px = r1 * IMAGE_SIZE as f32;
-        let py = r2 * IMAGE_SIZE as f32;
+            // Convert u, v coordinates to a complex number. 
+            // This directly maps x position of `uv` to real component of `c`
+            // and y position of `uv` to imaginary component of `c`.
+            let mut c = Complex::from(uv);
 
-        let m = mandelbrot(c, NUM_ITERATIONS);
+            // Transform `c` into the correct frame.
+            c = transform(c);
 
-        let mc = (m * 255.0) as u8;
-        im.put_pixel(px as u32, py as u32, Rgb([mc, mc, mc]));
+            // Evaluate the mandelbrot set at `c`.
+            let m = mandelbrot(c);
+
+            // Plot the result at the pixel coordinates.
+            im.put_pixel(x, y, Color::grayscale(m));
+        }
     }
 
+    // Save the image to the file 'mandelbrot.png'
     im.save("mandelbrot.png").unwrap()
 }
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,83 @@
+#![allow(unused)]
+
 use std::ops::{Add, Div, Mul, Sub};
 
+use image::{ImageBuffer, Pixel, PixelWithColorType, Rgb};
+
+pub type MandelbrotImage = ImageBuffer<Rgb<u8>, Vec<u8>>;
+
+/// A nice abstraction to handle instantiating various colors.
+#[derive(Debug, Clone, Copy)]
+pub struct Color;
+
+impl Color {
+    /// Create a new grayscale color from a single luminance value.
+    pub fn grayscale(l: f32) -> Rgb<u8> {
+        assert!((0.0..=1.0).contains(&l));
+        let v = (l * 255.0) as u8;
+
+        Rgb([v, v, v])
+    }
+
+    /// Create a color from the given `r`, `g`, and `b` values.
+    pub fn rgb(r: f32, g: f32, b: f32) -> Rgb<u8> {
+        assert!((0.0..=1.0).contains(&r));
+        assert!((0.0..=1.0).contains(&g));
+        assert!((0.0..=1.0).contains(&b));
+
+        Rgb([
+            (r * 255.0) as u8,
+            (g * 255.0) as u8,
+            (b * 255.0) as u8,
+        ])
+    }
+}
+
+/// A point in 2D space.
+#[derive(Clone, Copy, Debug)]
+pub struct Point<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> Point<T> {
+    /// Creates a new instance of `Point` given an x and y position.
+    #[inline]
+    pub const fn new(x: T, y: T) -> Point<T> {
+        Self { x, y }
+    }
+
+    /// Maps a function over the x and y parts of this point.
+    #[inline]
+    pub fn map<F: Fn(T) -> U, U>(self, f: F) -> Point<U> {
+        Point::<U> {
+            x: f(self.x),
+            y: f(self.y),
+        }
+    }
+}
+
+impl Point<u32> {
+    /// Converts a point from pixel coordinates to uv coordinates given the size of the image.
+    #[inline]
+    pub fn to_uv(self, n: u32) -> Point<f32> {
+        self.map(|v| v as f32 / n as f32)
+    }
+}
+
+impl<T: Default> Default for Point<T> {
+    fn default() -> Self {
+        Self {
+            x: T::default(),
+            y: T::default(),
+        }
+    }
+}
+
+/// A complex number of type `f32`.
+pub type ComplexF32 = Complex<f32>;
+
+/// A complex number.
 #[derive(Clone, Copy, Debug)]
 pub struct Complex<T> {
     pub re: T,
@@ -9,7 +87,7 @@ pub struct Complex<T> {
 impl<T> Complex<T> {
     /// Creates a new instance of `Complex` with the given real and imaginary parts.
     #[inline]
-    pub fn new(re: T, im: T) -> Complex<T> {
+    pub const fn new(re: T, im: T) -> Complex<T> {
         Self { re, im }
     }
     
@@ -32,11 +110,24 @@ impl<T> Complex<T> {
     }
 }
 
+impl<T> From<Point<T>> for Complex<T> {
+    /// Converts a point to a complex number by directly
+    /// mapping the point's x-value to the real component
+    /// and the point's y-value to the imaginary component.
+    #[inline]
+    fn from(value: Point<T>) -> Self {
+        Self {
+            re: value.x,
+            im: value.y,
+        }
+    }
+}
+
 impl<T: Default> Default for Complex<T> {
     fn default() -> Self {
         Self {
-            re: Default::default(),
-            im: Default::default(),
+            re: T::default(),
+            im: T::default(),
         }
     }
 }
@@ -94,17 +185,20 @@ impl<T: Clone + Copy + Div<T, Output = T> + Add<T, Output = T> + Sub<T, Output =
 }
 
 impl<T> From<(T, T)> for Complex<T> {
+    /// Converts a tuple into a complex number.
     fn from(value: (T, T)) -> Self {
         Complex::new(value.0, value.1)
     }
 }
 
 impl<T> From<Complex<T>> for (T, T) {
+    /// Converts a complex number into a tuple.
     fn from(value: Complex<T>) -> Self {
         (value.re, value.im)
     }
 }
 
+#[allow(private_bounds)]
 impl<T: Hypot> Complex<T> {
     /// Computes the absolute value (magnitude) of a complex number.
     #[inline]
@@ -114,7 +208,7 @@ impl<T: Hypot> Complex<T> {
 }
 
 
-pub trait Hypot {
+trait Hypot {
     fn hypotenuse(self, rhs: Self) -> Self;
 }
 
@@ -132,12 +226,13 @@ impl Hypot for f64 {
 
 
 macro_rules! impl_op_real {
-    ($($op:tt, $fn:ident, $trait:ident);*) => {
+    ($($op:tt, $fn:ident, $trait:ident, $doc:tt);*) => {
         $(
             impl<T: Clone + Copy + $trait<T, Output = T>> $trait<T> for Complex<T> {
                 type Output = Self;
 
                 #[inline]
+                #[doc = $doc]
                 fn $fn(self, rhs: T) -> Self::Output {
                     Self {
                         re: self.re $op rhs,
@@ -149,4 +244,9 @@ macro_rules! impl_op_real {
     }
 }
 
-impl_op_real!(+, add, Add; -, sub, Sub; *, mul, Mul; /, div, Div);
+impl_op_real!(
+    +, add, Add, "Adds a real number to both the real and imaginary components of this complex number."; 
+    -, sub, Sub, "Subtracts a real number from both the real and imaginary components of this complex number."; 
+    *, mul, Mul, "Multiplies both the real and imaginary components of this complex number by a real number, effectively scaling it."; 
+    /, div, Div, "Divides both the real and imaginary components of this complex number by a real number, effectively scaling it."
+);
